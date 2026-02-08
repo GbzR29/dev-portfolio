6d6ef600c44946919b282894f398bc2c93eca037
Su4amp <gabrielfeliperc@hotmail.com>
2026-02-08 01:00:43 -0300

    added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,36 @@ import { ProjectsSection } from "@/components/sections/ProjectsSection";
 import { ContactSection } from "@/components/sections/ContactSection";
 
 import TriangleParticles from "@/components/particles/TriangleParticles";
-import HeroCard from "@/components/cards/HeroCard";
 
 
+export const metadata = {
+  title: 'Gabriel Carvalho | My web portfolio', 
+  description: 'Welcome to my online corner, ',
+  
+  openGraph: {
+    title: 'Seu Nome | Portfolio & UI Design',
+    description: 'Welcome to my digital corner!',
+    url: 'https://www.gabrielfrc.dev/',
+    siteName: 'GabrielFRC.dev',
+    images: [
+      {
+        url: '/logo.png', // Deve estar na pasta /public
+        width: 1200,
+        height: 630,
+        alt: 'Banner preview',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+
+  /*twitter: {
+    card: 'summary_large_image',
+    title: 'Seu Nome | Portfolio & UI Design',
+    description: 'Portfolio focado em interfaces modernas e animações fluidas.',
+    images: ['/banner.png'], // Opcional: pode ser uma imagem diferente para o Twitter
+  },*/
+};
 
 export default function Home() {
   return (
